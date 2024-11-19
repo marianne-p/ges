1bafe908181c6989797a54bdc5d09a0682240a32
Mariana Pihur <mariana1pihur@gmail.com>
2024-11-19 14:40:55 +0000

    root changed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => (
   <ErrorBoundary>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Router basename="/ges/">
+        <Router basename="/">
           <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/comparison" element={<Comparison />} />
